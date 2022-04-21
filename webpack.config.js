55c850c205ea2f31f298ea265bed16565989bb8e
boping <344114999@qq.com>
2022-04-21 18:13:55 +0800

    feat: Init webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src",
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js",
+  },
+  module: {},
+  plugins: [],
+};
