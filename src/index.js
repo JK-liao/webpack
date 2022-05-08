f21f8bb12cd01bb6faf75dd2fac634477fd97a6c
boping <344114999@qq.com>
2022-05-08 08:00:32 +0800

    feat: 图片，iconfont

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+// style
 import './global.css';
 import './style/pc.scss';
 // import './style/mobile.scss';
+import './assets/iconfonts/guangzhouzhixun/iconfont.css';
+
+// js
 import ajax from './utils/ajax';
 import deepClone from './utils/deepClone';
+
+// img
+import bossImage from './assets/imgs/boss.jpg';
+(function() {
+  const user = document.querySelector('header .user-info');
+  if (!user) {
+    return;
+  }
+  const avater = new Image();
+  avater.classList.add('user-avater');
+  avater.src = bossImage;
+  user.insertBefore(avater, user.childNodes[0]);
+}())
 
 ajax
   .get('http://localhost:7001/user/query?phone=13386126957')
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
     ],
   },
   plugins: [],
